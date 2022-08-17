c18e80516408115648a226f53c5484ff5de5bdfe
cfdebug <rfadventure22@gmail.com>
2022-08-16 20:10:41 -0500

    Completed Activity and Bonus

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
-
+// have the NPC continuously walking
+async function moveNPC(){
+while(true){
+await npc.walkNorth(1400);
+await npc.walkEast(1200);
+await npc.walkSouth(300);
+await npc.walkEast(1500);
+await npc.walkSouth(1500);
+await npc.walkWest(2700);
+await npc.walkNorth(400);
+}
+}
+moveNPC();
 // Create the inventory
 const inventory = newInventory()
 move(inventory).to(0, 0)
